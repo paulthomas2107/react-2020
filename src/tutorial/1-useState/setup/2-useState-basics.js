682e20bed4ed7097ac14dc16ce0a81b58fd3c1c3
Paul Thomas <paulthomas2107@gmail.com>
2021-04-02 21:20:24 +0100

    more

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //console.log(useState("Hello Paul"));
+  //const value = useState(217)[0];
+  //const handler = useState(217)[1];
+  //console.log(value, handler);
+  const [text, setText] = useState("random title !");
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("Hello Paul !!");
+    } else {    
+      setText("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
